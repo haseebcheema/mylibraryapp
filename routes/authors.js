7489a140385194f24d4c75887f9e85342bb80905
haseebcheema <ihaseebtariq1@gmail.com>
2023-05-31 14:05:08 +0500

    implemented search and creation for authors

--- routes/authors.js
+++ routes/authors.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const Author = require('../model/author');
+const { default: mongoose } = require('mongoose');
 
 // get all authors
-router.get('/', (req, res) => {
-    res.render('authors/index');
+router.get('/', async (req, res) => {
+    let searchOptions = {};
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i');
+    }
+    try{
+        const authors = await Author.find(searchOptions);
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query 
+        });
+    } catch(err){
+        res.render('/');
+    }
 });
 
 // get form where new author is created
@@ -14,8 +27,20 @@ router.get('/new', (req, res) => {
 });
 
 // to create a new author
-router.post('/', (req, res) => {
-    res.send(req.body.name);
+router.post('/', async (req, res) => {
+    try {
+        const author = new Author({
+            name: req.body.name
+        });
+        const newAuthor = await author.save();
+        res.redirect('/authors');
+    } catch (err) {
+        res.render('authors/new', {
+            author: req.body,
+            errorMessage: 'Error creating Author'
+        });
+    }    
 });
+
 
 module.exports = router;
