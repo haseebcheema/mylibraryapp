85686857bf26b6e944ddfc6b37ce0bc0c80fe3aa
haseebcheema <ihaseebtariq1@gmail.com>
2023-06-01 12:44:02 +0500

    some minor changes to resolve errors

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const Booj = require('../model/book');
+const Book = require('../model/book');
 
 router.get('/', async (req, res) => {
-    let books;
-    try {
-        books = await Book.find().sort( { createdAt: 'desc' } ).limit(10).exec;
-    } catch (error) {
-        books = [];
-    }
-    res.render('index', { books: books });
-});
+  let books;
+  try {
+    books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec();
+  } catch {
+    books = [];
+  }
+  res.render('index', { books: books });
+})
 
 module.exports = router;
